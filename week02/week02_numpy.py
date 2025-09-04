9b4a2ba19ea73a57dbe6998f4b016953670c9b2d
voidgogo <2021038@daelim.ac.kr>
2025-09-04 16:09:33 +0900

    v0.7 numpy

--- week02/week02_numpy.py
+++ week02/week02_numpy.py
@@ -1,24 +1,11 @@
 import numpy as np
 
-arr = np.arange(1, 6)
-print(arr)
+a = np.array([3, 2, 1])
+b = np.array([6, 5, 4])
 
-# indexing
-print(arr[2])
-print(arr[4], arr[-1], arr[len(arr)-1])
-
-# slicing
-print(arr[1:3])  # [2 3]
-print(arr[::3])  # [1 4]
-
-# 2d 배열
-arr2d = np.array(
-    [
-        [3, 2, 1],
-        [4, 5, 6],
-        [14, -5, 76]
-    ]
-)
-print(arr2d)
-print(arr2d[1, 2])  # 6
-print(arr2d[:, 1])  # [2  5 -5] 모든 행의 1열(0열~2열)
+# 산술연산
+print(a+b)
+print(a*b)
+print(b**2)
+print(a+10)
+print(b*2)
