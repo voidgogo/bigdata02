df524c6d31eddb79fd089e4a46de42505eaca675
voidgogo <2021038@daelim.ac.kr>
2025-09-04 16:06:35 +0900

    v0.6 numpy

--- week02/week02_numpy.py
+++ week02/week02_numpy.py
@@ -1,21 +1,24 @@
 import numpy as np
 
-# arr = np.array(["Korean", "English", "Mathmatics"])
-# arr = np.array([[1, 2, 3],[4, 5, 9]])
-arr = np.array(
+arr = np.arange(1, 6)
+print(arr)
+
+# indexing
+print(arr[2])
+print(arr[4], arr[-1], arr[len(arr)-1])
+
+# slicing
+print(arr[1:3])  # [2 3]
+print(arr[::3])  # [1 4]
+
+# 2d 배열
+arr2d = np.array(
     [
-        [
-            [1.0, 2.0, 3.1],
-            [4.2, 5.9, 9.1]
-        ],
-        [
-            [1.1, 0.8, 2.1],
-            [6.3, 1.9, 77.1]
-        ]
+        [3, 2, 1],
+        [4, 5, 6],
+        [14, -5, 76]
     ]
 )
-print(arr)
-print(arr.shape, arr.dtype, arr.ndim, arr.size)
-# 위 3차원 배열에서 1.9만 출력
-print(arr[1][1][1])
-print(arr[1, 1, 1])
+print(arr2d)
+print(arr2d[1, 2])  # 6
+print(arr2d[:, 1])  # [2  5 -5] 모든 행의 1열(0열~2열)
