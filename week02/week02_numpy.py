1bfa318524c6db1d3dd6e2a78044b504a2add8c0
voidgogo <2021038@daelim.ac.kr>
2025-09-04 15:19:52 +0900

    v0.4 numpy

--- week02/week02_numpy.py
+++ week02/week02_numpy.py
@@ -1,14 +1,19 @@
-# 넘파이(Numpy)
-# 써드파티 라이브러리 (별도의 설치가 필요, 단 colab에는 기본 설치가 되어 있음)
-# 파이썬에서 수치 계산을 위한 핵심 라이브러리
-# 다차원 배열 객체와 이를 다루기 위한 다양한 함수를 제공
-
 import numpy as np
 
-list1 = [1,2,3,4,5]
-array1 = np.array(list1)  # 파이썬 리스트를 넘파이 배열로 변환
-print(list1, type(list1))
-print(array1, type(array1))
+zeros = np.zeros((3,4))
+# zeros = np.array([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], dtype=float)
+print(zeros)
 
-array2 = np.array([[1,2,3],[4,5,6]])
-print(array2, type(array2))
+ones = np.ones((3,5))
+# ones = np.array([[1, 1, 1, 1, 1],[1, 1, 1, 1, 1],[1, 1, 1, 1, 1]], dtype=float)
+print(ones)
+
+# range_array = np.arange(0, 20, 2)
+range_array = np.arange(1, 10)
+# range_array = list()
+# for i in range(1, 10):
+#     range_array.append(i)
+print(range_array)
+
+space_array = np.linspace(0, 1, 5)  # 0부터 1까지 숫자 5개로 구간 설정
+print(space_array)
