70fd39919d3c142eda9eeb4406ae91e6a5428996
voidgogo <2021038@daelim.ac.kr>
2025-09-04 16:20:29 +0900

    v0.8 numpy

--- week02/week02_numpy.py
+++ week02/week02_numpy.py
@@ -1,11 +1,18 @@
 import numpy as np
 
-a = np.array([3, 2, 1])
-b = np.array([6, 5, 4])
+c = np.arange(1, 13)  # 1차원 배열
 
-# 산술연산
-print(a+b)
-print(a*b)
-print(b**2)
-print(a+10)
-print(b*2)
+# r = c.reshape(2, 2, 3)  # 3차원 배열형태로 변환
+r = c.reshape(4, 3)  # 2차원 배열형태로 변환
+print(c)
+print(r)
+f = r.flatten()  # 2차원 배열을 1차원 배열로 변환
+print(f)
+
+# 전치행렬
+t = r.T
+print(t)
+
+# 단위행렬
+e1 = np.eye(4)
+print(e1)
