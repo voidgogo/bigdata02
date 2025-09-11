5c402a44b09ff988fda0debaff4ba19ae21998b3
voidgogo <2021038@daelim.ac.kr>
2025-09-11 15:23:35 +0900

    v0.4 pandas

--- week03/week03_pandas.py
+++ week03/week03_pandas.py
@@ -1,12 +1,19 @@
 import pandas as pd
 
 df4 = pd.DataFrame({
-    '날짜':['2025-09-11','2025-09-11','2025-09-12','2025-09-12'],
-    '도시':['서울', '안양', '서울', '안양'],
-    '온도':[23, 22, 24, 26]},
+    # 'date':['2025-09-11','2025-09-11','2025-09-12','2025-09-13'],
+    'date':['2025-09-11','2025-09-11','2025-09-12','2025-09-12'],
+    'city':['서울', '안양', '서울', '안양'],
+    'temp':[23, 22, 24, 22]},
     index=[1, 2, 3, 4]
 )
 print(df4)
-# df6 = df4.sort_values('온도')  # ascending
-df6 = df4.sort_values('온도', ascending=False)  # descending
+df6 = df4.pivot(index='date', columns='city', values='temp')
 print(df6)
+
+# df7 = df4.drop(columns=['date', 'city'])  # drop specific columns
+# print(df7)
+
+# df8 = df4[df4.temp <= 23]
+# df8 = df4.duplicated()
+# print(df8)
