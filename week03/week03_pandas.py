935268757422a73098c6dc8cda3900597f8dcf87
voidgogo <2021038@daelim.ac.kr>
2025-09-11 15:29:01 +0900

    v0.5 pandas

--- week03/week03_pandas.py
+++ week03/week03_pandas.py
@@ -1,19 +1,15 @@
 import pandas as pd
 
 df4 = pd.DataFrame({
-    # 'date':['2025-09-11','2025-09-11','2025-09-12','2025-09-13'],
     'date':['2025-09-11','2025-09-11','2025-09-12','2025-09-12'],
     'city':['서울', '안양', '서울', '안양'],
-    'temp':[23, 22, 24, 22]},
+    'temp':[23, 22, 24, 26]},
     index=[1, 2, 3, 4]
 )
 print(df4)
-df6 = df4.pivot(index='date', columns='city', values='temp')
-print(df6)
-
-# df7 = df4.drop(columns=['date', 'city'])  # drop specific columns
-# print(df7)
-
-# df8 = df4[df4.temp <= 23]
-# df8 = df4.duplicated()
-# print(df8)
+df9 = df4.sample(n=2)  # Randomly select n rows.
+# print(df9)
+# df10 = df4.nsmallest(2, 'temp')
+df10 = df4.nlargest(2, 'temp')
+# print(df10)
+print(df4.head(3))
