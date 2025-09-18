8029bc9da4f039c656afd786f814693e2f2ed65d
voidgogo <2021038@daelim.ac.kr>
2025-09-18 16:45:33 +0900

    v1.3 pandas NaN

--- week04/week04_pandas.py
+++ week04/week04_pandas.py
@@ -7,11 +7,15 @@ mpg = sns.load_dataset("mpg")
 # print(mpg[mpg['horsepower'].isnull()])
 
 # 결측치 처리 #1
-mpg.dropna(inplace=True)  # mpg = mpg.dropna()
-print(mpg[mpg['horsepower'].isnull()])
-print(mpg.info())
+# mpg.dropna(inplace=True)  # mpg = mpg.dropna()
+# print(mpg[mpg['horsepower'].isnull()])
+# print(mpg.info())
 
 # 결측치 처리 #2
 # mpg = mpg.fillna(mpg['horsepower'].mean())
 # print(mpg[mpg['horsepower'].isnull()])
 # print(mpg.info())
+
+# 결측치 처리 #3
+mpg.drop(columns=['horsepower'], inplace=True)
+print(mpg.info())
