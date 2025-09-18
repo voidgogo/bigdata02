4b2fdeeb93cd2fccb16bdfb5eb1219a8c0261b33
voidgogo <2021038@daelim.ac.kr>
2025-09-18 19:23:46 +0900

    week07 v0.1 visualization

--- week07/week07_pandas.py
+++ week07/week07_pandas.py
@@ -1,10 +1,39 @@
-import pandas as pd
 import seaborn as sns
+from matplotlib import pyplot as plt
 
-# print(len(sns.get_dataset_names()))
 titanic = sns.load_dataset("titanic")
-# print(titanic.info())
-# print(titanic.describe())
-# print(titanic[titanic['age'] <= 15])
-# print(titanic.head())
-print(titanic.nlargest(5, 'age'))
+plt.figure(figsize = (15,12))
+
+plt.subplot(2, 3, 1)
+sns.boxplot(data=titanic, x="survived", y="age")
+plt.title("Age Distribution by Survival Status")
+plt.xlabel("Survived (0: No, 1: Yes)")
+plt.ylabel("Age")
+
+plt.subplot(2, 3, 2)
+sns.boxplot(data=titanic, x="sex", y="age")
+plt.title("Age Distribution by Gender")
+plt.xlabel("Gender")
+plt.ylabel("Age")
+
+plt.subplot(2, 3, 3)
+sns.boxplot(data=titanic, x="class", y="fare")
+plt.title("Fare Distribution by Class")
+plt.xlabel("Passenger Class")
+plt.ylabel("Fare")
+
+plt.subplot(2, 3, 4)
+sns.boxplot(data=titanic, x="survived", y="fare")
+plt.title("Fare Distribution by Survival Status")
+plt.xlabel("Survived (0: No, 1: Yes)")
+plt.ylabel("Fare")
+
+plt.subplot(2, 3, 5)
+sns.boxplot(data=titanic, x="sex", y="age", hue="survived", palette="Set2")
+plt.title("Age Distribution by Survival Status")
+plt.title("Age by Gender and Survival")
+plt.xlabel("Gender")
+plt.ylabel("Age")
+plt.legend(title="Survived", labels=["No", "Yes"])
+
+plt.show()
