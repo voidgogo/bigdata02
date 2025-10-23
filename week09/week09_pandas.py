bbd73e3e74e0a4deb8bed4d842d47e4f22f08019
voidgogo <2021038@daelim.ac.kr>
2025-10-23 16:16:13 +0900

    v2.0 pandas

--- week09/week09_pandas.py
+++ week09/week09_pandas.py
@@ -1,21 +1,12 @@
 import pandas as pd
 
 uber = pd.read_csv("uber.csv")
-# uber['START_DATE*'] = pd.to_datetime(uber['START_DATE*'])
-# print(uber)
 uber['START_DATE*'] = pd.to_datetime(uber['START_DATE*'], errors='coerce')
 uber['END_DATE*'] = pd.to_datetime(uber['END_DATE*'], errors='coerce')
 uber = uber.sort_values(['START_DATE*', 'END_DATE*'])
-# print(uber.info())
-# print(uber['START_DATE*'])
-# print(uber['START_DATE*'].unique())
 uber = uber.drop(1155)
-# print(uber['END_DATE*'].unique())
-# print(uber[uber['MILES*'] == uber['MILES*'].min()])
-# print(uber['PURPOSE*'].value_counts())
-# print(uber['PURPOSE*'].isna().sum())
-
-# uber['DURATION*'] = (uber['END_DATE*'] - uber['START_DATE*'])
 uber['DURATION*'] = (uber['END_DATE*'] - uber['START_DATE*']).dt.total_seconds() / 60
-# print(uber['DURATION*'])
-print(uber.info())
+# print(uber['CATEGORY*'].value_counts())
+# print(uber.groupby(['CATEGORY*', 'PURPOSE*']))
+print(uber.groupby(['CATEGORY*', 'PURPOSE*'])[['MILES*','DURATION*']].agg(['mean', 'median', 'count']))
+print(uber[uber['PURPOSE*'] == 'Airport/Travel'][['MILES*', 'DURATION*']])
