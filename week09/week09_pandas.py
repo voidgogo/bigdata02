f16d0149f60dc697383eea9c9a3e41ba81ef373c
voidgogo <2021038@daelim.ac.kr>
2025-10-23 15:50:43 +0900

    v1.9 pandas

--- week09/week09_pandas.py
+++ week09/week09_pandas.py
@@ -1,14 +1,21 @@
 import pandas as pd
 
 uber = pd.read_csv("uber.csv")
-# print(type(uber))
-# print(uber.head(7))
+# uber['START_DATE*'] = pd.to_datetime(uber['START_DATE*'])
+# print(uber)
+uber['START_DATE*'] = pd.to_datetime(uber['START_DATE*'], errors='coerce')
+uber['END_DATE*'] = pd.to_datetime(uber['END_DATE*'], errors='coerce')
+uber = uber.sort_values(['START_DATE*', 'END_DATE*'])
 # print(uber.info())
-print(uber.describe())
-
-# print(uber[uber["MILES*"] == 12204.7])  # describe
-# print(uber[uber["MILES*"] == uber["MILES*"].max()])
-# print(uber.tail())
+# print(uber['START_DATE*'])
+# print(uber['START_DATE*'].unique())
 uber = uber.drop(1155)
-# print(uber.tail())
-print(uber.describe())
+# print(uber['END_DATE*'].unique())
+# print(uber[uber['MILES*'] == uber['MILES*'].min()])
+# print(uber['PURPOSE*'].value_counts())
+# print(uber['PURPOSE*'].isna().sum())
+
+# uber['DURATION*'] = (uber['END_DATE*'] - uber['START_DATE*'])
+uber['DURATION*'] = (uber['END_DATE*'] - uber['START_DATE*']).dt.total_seconds() / 60
+# print(uber['DURATION*'])
+print(uber.info())
