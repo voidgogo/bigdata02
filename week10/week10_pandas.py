336bae72fdfa180d6a57c7d25dcd885515d475bc
voidgogo <2021038@daelim.ac.kr>
2025-10-30 15:34:33 +0900

    v2.3 pandas

--- week10/week10_pandas.py
+++ week10/week10_pandas.py
@@ -3,14 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-german = pd.read_csv('german.csv')
-# print(german.info())
-# print(german.head())
-# print(german.describe())
-# print(german['Age'].describe())
-# print(pd.cut(german['Age'], bins=8))
-# print(pd.cut(german['Age'], bins=8).reset_index().groupby('Age').count())
-ages = [10, 20, 30, 40, 50, 60, 70, 80]
-# print(pd.cut(german['Age'], bins=ages).reset_index().groupby('Age').count())
-print(pd.cut(german['Age'], bins=ages, right=False).reset_index().groupby('Age').count())
-# print(german[german['Age'] == 20])
+korean = pd.read_csv('korean.csv')
+# print(korean['Age'].value_counts().sort_values(ascending=False))
+print(pd.qcut(korean['Age'], q=8).reset_index().groupby('Age').size())
