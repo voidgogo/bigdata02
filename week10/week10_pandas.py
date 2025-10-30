b0d1dc7360a1355a784ee3750d4c646a08ecaea4
voidgogo <2021038@daelim.ac.kr>
2025-10-30 15:58:13 +0900

    v2.4 pandas

--- week10/week10_pandas.py
+++ week10/week10_pandas.py
@@ -3,6 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-korean = pd.read_csv('korean.csv')
-# print(korean['Age'].value_counts().sort_values(ascending=False))
-print(pd.qcut(korean['Age'], q=8).reset_index().groupby('Age').size())
+s1 = pd.Series([0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0])
+# print(s1)
+sc1 = s1.cumsum()  # cumulative sum
+# print(sc1)
+# print(s1.mul(sc1))  # multiply
+# print(s1.mul(sc1).diff())  # difference
+# print(s1.mul(sc1).diff().where(lambda n: n>0))  # where (if)
+# print(s1.mul(sc1).diff().where(lambda n: n > 0).ffill())  # front fill
+print(s1.mul(sc1).diff().where(lambda n: n > 0).ffill().add(sc1, fill_value=0))  # add 0
