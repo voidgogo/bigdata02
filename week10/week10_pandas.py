e267a98007cd806a5b4d010a936aa8ce0424858e
voidgogo <2021038@daelim.ac.kr>
2025-10-30 16:26:18 +0900

    v0.5 visualization

--- week10/week10_pandas.py
+++ week10/week10_pandas.py
@@ -1,14 +1,11 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-s1 = pd.Series([0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0])
-# print(s1)
-sc1 = s1.cumsum()  # cumulative sum
-# print(sc1)
-# print(s1.mul(sc1))  # multiply
-# print(s1.mul(sc1).diff())  # difference
-# print(s1.mul(sc1).diff().where(lambda n: n>0))  # where (if)
-# print(s1.mul(sc1).diff().where(lambda n: n > 0).ffill())  # front fill
-print(s1.mul(sc1).diff().where(lambda n: n > 0).ffill().add(sc1, fill_value=0))  # add 0
+wh = pd.read_csv('wh.csv')
+# print(wh.info())
+# print(wh.head())
+# print(wh.describe())
+sns.scatterplot(x='Weight', y='Height', data=wh)
+plt.show()
