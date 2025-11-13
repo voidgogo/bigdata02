908e84a6e27f05baa1536ea16882f0909511e1a9
voidgogo <2021038@daelim.ac.kr>
2025-11-13 15:21:49 +0900

    v2.9 pandas

--- week12/week12_pandas.py
+++ week12/week12_pandas.py
@@ -2,26 +2,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import scipy.stats as stats
 
-wh = pd.read_csv('wh.csv')
-# new_wh = wh.query('Weight < 390')
-# print(new_wh.info())
-# print(new_wh.describe())
-
-criteria = wh['Weight'].quantile(0.9999)  # 99.99% 이상인 데이터 지점
-# print(criteria)  # 약 255.9 파운드
-# print(round(criteria, 1))  # 255.9
-new_wh = wh[wh['Weight'] < criteria]
-# print(new_wh.info())
-# print(new_wh.describe())
-# sns.scatterplot(x='Weight', y='Height', data=new_wh)
-# plt.show()
-
-# print(wh[wh['Weight'] > criteria])
-# print(wh[wh['Weight'] > criteria].index)
-
-wh['Weight'] = new_wh['Weight'].clip(100, 200) # lower bound, upper bound
-# print(wh.tail())
-# print(wh.info())
-sns.scatterplot(x='Weight', y='Height', data=wh)
-plt.show()
+s = pd.Series([1, np.nan, 3, np.nan, 5, 6, np.nan, np.nan, 9])
+print(s)
+# 내삽(보간)
+print(s.interpolate(method='spline', order=1, limit=1, limit_direction='both'))
