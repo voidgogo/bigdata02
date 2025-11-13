173f0a519d4f704b3b40b2338d1bb2b14e62863d
voidgogo <2021038@daelim.ac.kr>
2025-11-13 16:14:40 +0900

    v3.2 pandas

--- week12/week12_pandas.py
+++ week12/week12_pandas.py
@@ -10,10 +10,18 @@ usc = pd.read_csv('us_corona19.csv')
 usc['Date'] = pd.to_datetime(usc['Date'])  # object -> datetime
 # print('='*20)
 # print(usc.info())
-usc = usc.set_index('Date').sort_values('Date')  # 인덱스를 날짜로 변경, 그리고 날짜 기준 오름차순으로 정렬
+
+# 인덱스를 날짜로 변경, 그리고 날짜 기준 오름차순으로 정렬
+usc = usc.set_index('Date').sort_values('Date')
 # print(usc)
 # print(usc['2021-09' : '2021-10'])  # 슬라이싱
 # print(usc.reset_index().set_index('Province/State').sort_index())
+
+# 인덱스 재변경, 그리고 날짜에서 주/자치량으로 오름차순(알파벳) 정렬
 usc = usc.reset_index().set_index('Province/State').sort_index()
 # print(usc.index)
 print(usc.index.unique(), len(usc.index.unique()))
+# print(usc['Alabama':'California'])
+# print(usc['Arizona':'California'])
+# print(usc['California':'Delaware'])
+print(usc['Ca':'Df'])
