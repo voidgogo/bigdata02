0340b2c85719107bd35ffa13b7e664e8895fd878
voidgogo <2021038@daelim.ac.kr>
2025-11-13 15:36:46 +0900

    v3.0 pandas

--- week12/week12_pandas.py
+++ week12/week12_pandas.py
@@ -4,7 +4,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy.stats as stats
 
-s = pd.Series([1, np.nan, 3, np.nan, 5, 6, np.nan, np.nan, 9])
-print(s)
-# 내삽(보간)
-print(s.interpolate(method='spline', order=1, limit=1, limit_direction='both'))
+usc = pd.read_csv('us_corona19.csv')
+# print(usc.info())
+# print(usc.describe())
+# print(usc['Admin2'])
+print(usc[usc['Admin2'].isna()])
+
+# print(usc.dropna())
+
+# new_usc = usc.fillna('Daelim')
+# print(new_usc.query('Admin2 == "Daelim"'))
