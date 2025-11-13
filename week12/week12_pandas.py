fa7a512c00069417f2aab3b884075b1b80b468e5
voidgogo <2021038@daelim.ac.kr>
2025-11-13 14:24:35 +0900

    v2.8 pandas

--- week12/week12_pandas.py
+++ week12/week12_pandas.py
@@ -4,11 +4,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 wh = pd.read_csv('wh.csv')
-# print(wh.info())
-# print(wh.head())
-# print(wh.describe())
-# 파운드 (1 == 453.592g), 인치 (1 == 2.54cm)
-print(wh.query('Weight > 390'))
-print(wh[wh['Weight'] > 390])
-# sns.scatterplot(x='Weight', y='Height', data=wh)
-# plt.show()
+# new_wh = wh.query('Weight < 390')
+# print(new_wh.info())
+# print(new_wh.describe())
+
+criteria = wh['Weight'].quantile(0.9999)  # 99.99% 이상인 데이터 지점
+print(criteria)  # 약 255.9 파운드
+print(round(criteria, 1))  # 255.9
+new_wh = wh[wh['Weight'] < criteria]
+print(new_wh.info())
