81eb434340e4780b267306313970a4054753e366
voidgogo <2021038@daelim.ac.kr>
2025-11-13 15:52:59 +0900

    v3.1 pandas

--- week12/week12_pandas.py
+++ week12/week12_pandas.py
@@ -4,13 +4,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy.stats as stats
 
+# 특정 기간동안 미국에서 코로나19 바이러스 확진자 수를 지역으로 나눠 정리한 데이터
 usc = pd.read_csv('us_corona19.csv')
 # print(usc.info())
-# print(usc.describe())
-# print(usc['Admin2'])
-print(usc[usc['Admin2'].isna()])
-
-# print(usc.dropna())
-
-# new_usc = usc.fillna('Daelim')
-# print(new_usc.query('Admin2 == "Daelim"'))
+usc['Date'] = pd.to_datetime(usc['Date'])  # object -> datetime
+# print('='*20)
+# print(usc.info())
+usc = usc.set_index('Date').sort_values('Date')  # 인덱스를 날짜로 변경, 그리고 날짜 기준 오름차순으로 정렬
+# print(usc)
+# print(usc['2021-09' : '2021-10'])  # 슬라이싱
+# print(usc.reset_index().set_index('Province/State').sort_index())
+usc = usc.reset_index().set_index('Province/State').sort_index()
+# print(usc.index)
+print(usc.index.unique(), len(usc.index.unique()))
