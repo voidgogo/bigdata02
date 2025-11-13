d89a3c5fef2786619581e9598738e8828fa40e46
voidgogo <2021038@daelim.ac.kr>
2025-11-13 14:43:34 +0900

    v1.0 visualization

--- week12/week12_pandas.py
+++ week12/week12_pandas.py
@@ -9,7 +9,13 @@ wh = pd.read_csv('wh.csv')
 # print(new_wh.describe())
 
 criteria = wh['Weight'].quantile(0.9999)  # 99.99% 이상인 데이터 지점
-print(criteria)  # 약 255.9 파운드
-print(round(criteria, 1))  # 255.9
+# print(criteria)  # 약 255.9 파운드
+# print(round(criteria, 1))  # 255.9
 new_wh = wh[wh['Weight'] < criteria]
-print(new_wh.info())
+# print(new_wh.info())
+# print(new_wh.describe())
+sns.scatterplot(x='Weight', y='Height', data=new_wh)
+plt.show()
+
+# print(wh[wh['Weight'] > criteria])
+# print(wh[wh['Weight'] > criteria].index)
