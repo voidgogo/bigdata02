97e0a53f3d7dcf989524fe2f0191e457c2394357
voidgogo <2021038@daelim.ac.kr>
2025-11-20 15:18:50 +0900

    v3.6 pandas

--- week13/week13_pandas.py
+++ week13/week13_pandas.py
@@ -3,21 +3,36 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 apple = pd.read_csv('APPL_price.csv')
+apple['Date'] = pd.to_datetime(apple['Date'])
+apple = apple.set_index('Date')
 # print(apple.head())
-# print(apple.describe())
-# print(apple.info())
-apple['Date'] = pd.to_datetime(apple['Date'])  # object -> datetime
-# print(apple.info())
-apple = apple.set_index('Date')  # index를 날짜시간 칼럼으로 대체
+# apple = apple.reset_index()
 # print(apple.head())
-# print(apple.tail())
+# print(apple.isnull().sum())  # 결측치는 0
+# print(apple['Close'].max())
+# print(apple['Close'].min())
+# print(apple['Close'].mean())
 
-# 시계열 데이터 슬라이싱
-# print(apple['2022-06-14':'2022-06-16'])
-# print(apple['2022-04':'2022-06'])
+# 일일 수익률 계산(%) 결과 칼럼 추가
+apple['Daily_Return'] = apple['Close'].pct_change() * 100
+# print(apple[['Close', 'Daily_Return']])
 
-print(apple['Close'])
-apple_1months = apple.resample('1M').mean()  # 일주일 단위로 행을 묶어 평균값 추출
-print(apple_1months['Close'])
+# 일일 가격 변화(달러)
+apple['Price_Change'] = apple['Close'].diff()
+# print(apple[['Close', 'Price_Change']])
+
+# 일일 가격 변동폭(%)
+apple['High_Low_Range'] = ((apple['High'] - apple['Low']) / apple['Low']) * 100
+# print(apple[['High','Low','High_Low_Range']])
+
+# 이동평균 (20일)
+apple['MA_20'] = apple['Close'].rolling(window=20).mean()
+# print(apple['MA_20'].head(20))
+
+# 변동성 (20일 표준편차)
+apple['Volatility'] = apple['Daily_Return'].rolling(window=20).std()
+# print(apple['Volatility'])
+
 # print(apple.info())
-# print(apple_7days.describe())
+apple = apple.reset_index()
+print(apple[['Date', 'Close', 'Daily_Return', 'MA_20', 'Volatility']].tail(10))
