0923e2f88e54a414f5e8da325e5e1e9f8472f443
voidgogo <2021038@daelim.ac.kr>
2025-11-20 14:43:00 +0900

    v3.5 pandas

--- week13/week13_pandas.py
+++ week13/week13_pandas.py
@@ -14,4 +14,10 @@ apple = apple.set_index('Date')  # index를 날짜시간 칼럼으로 대체
 
 # 시계열 데이터 슬라이싱
 # print(apple['2022-06-14':'2022-06-16'])
-print(apple['2022-04':'2022-06'])
+# print(apple['2022-04':'2022-06'])
+
+print(apple['Close'])
+apple_1months = apple.resample('1M').mean()  # 일주일 단위로 행을 묶어 평균값 추출
+print(apple_1months['Close'])
+# print(apple.info())
+# print(apple_7days.describe())
