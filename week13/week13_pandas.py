991bd11da49749b43ee3aeaebfbb7785004edb38
voidgogo <2021038@daelim.ac.kr>
2025-11-20 16:16:11 +0900

    v3.8 pandas

--- week13/week13_pandas.py
+++ week13/week13_pandas.py
@@ -11,7 +11,6 @@ apple['High_Low_Range'] = ((apple['High'] - apple['Low']) / apple['Low']) * 100
 apple['MA_20'] = apple['Close'].rolling(window=20).mean()
 apple['Volatility'] = apple['Daily_Return'].rolling(window=20).std()
 apple = apple.reset_index()
-# print(apple[['Date', 'Close', 'Daily_Return', 'MA_20', 'Volatility']].tail(10))
 apple['Year'] = apple['Date'].dt.year
 yearly_stats = apple.groupby('Year')['Close'].agg([
     ('최고', 'max'),
@@ -19,23 +18,31 @@ yearly_stats = apple.groupby('Year')['Close'].agg([
     ('평균', 'mean'),
     ('개수', 'count'),
 ])
-# print(yearly_stats.tail(9))
-
 apple['Month'] = apple['Date'].dt.month
 monthly_stats = apple.groupby('Month')['Close'].mean()
-# print(monthly_stats)
-
 # 분기별 수익율 분석
 apple['Quarter'] = apple['Date'].dt.quarter
 quarterly_return = apple.groupby(['Year', 'Quarter'])['Daily_Return'].sum()
-# print(quarterly_return)
-
 # 거래량이 많은 날 분석
-# print(apple['Volume'])
 apple['Volume_Category'] = pd.cut(apple['Volume'],
                                   bins=[0, 100000000, 300_000_000, 5e8, 10e10],
                                   labels=['Low', 'Medium', 'High', 'Very High']
                                   )
 volume_analysis = apple.groupby(['Volume_Category'])['Close'].agg(['count', 'mean'])
-print(volume_analysis)
+# 최근 2년 데이터
+recent_2year = apple[apple['Date'] >= '2020-06-17']
+# 가격 100달러 이상
+high_price_days =apple[apple['Close'] >= 100]
+# 수익율이 양수인 날
+positive_return = apple[apple['Daily_Return'] > 0]
+# print(f"{len(positive_return) / len(apple) * 100:.2f}")
+# 거래량이 상위 5%에 해당하는 날
+high_volume_threshold = apple['Volume'].quantile(0.95)
+# print(high_volume_threshold)
+high_volume_days = apple[apple['Volume'] >= high_volume_threshold]
+# print(high_volume_days)
 
+apple_filtered = apple[(apple['Date'] >= '2020-01-01') &
+                       (apple['Close'] > 50) &
+                       (apple['Volume'] > 2e8)]
+print(apple_filtered)
