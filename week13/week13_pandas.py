92e631cc980f8a919585572f1cf55fa7e8024050
voidgogo <2021038@daelim.ac.kr>
2025-11-20 15:44:01 +0900

    v3.7 pandas

--- week13/week13_pandas.py
+++ week13/week13_pandas.py
@@ -5,34 +5,37 @@ import matplotlib.pyplot as plt
 apple = pd.read_csv('APPL_price.csv')
 apple['Date'] = pd.to_datetime(apple['Date'])
 apple = apple.set_index('Date')
-# print(apple.head())
-# apple = apple.reset_index()
-# print(apple.head())
-# print(apple.isnull().sum())  # 결측치는 0
-# print(apple['Close'].max())
-# print(apple['Close'].min())
-# print(apple['Close'].mean())
-
-# 일일 수익률 계산(%) 결과 칼럼 추가
 apple['Daily_Return'] = apple['Close'].pct_change() * 100
-# print(apple[['Close', 'Daily_Return']])
-
-# 일일 가격 변화(달러)
 apple['Price_Change'] = apple['Close'].diff()
-# print(apple[['Close', 'Price_Change']])
-
-# 일일 가격 변동폭(%)
 apple['High_Low_Range'] = ((apple['High'] - apple['Low']) / apple['Low']) * 100
-# print(apple[['High','Low','High_Low_Range']])
-
-# 이동평균 (20일)
 apple['MA_20'] = apple['Close'].rolling(window=20).mean()
-# print(apple['MA_20'].head(20))
-
-# 변동성 (20일 표준편차)
 apple['Volatility'] = apple['Daily_Return'].rolling(window=20).std()
-# print(apple['Volatility'])
-
-# print(apple.info())
 apple = apple.reset_index()
-print(apple[['Date', 'Close', 'Daily_Return', 'MA_20', 'Volatility']].tail(10))
+# print(apple[['Date', 'Close', 'Daily_Return', 'MA_20', 'Volatility']].tail(10))
+apple['Year'] = apple['Date'].dt.year
+yearly_stats = apple.groupby('Year')['Close'].agg([
+    ('최고', 'max'),
+    ('최저', 'min'),
+    ('평균', 'mean'),
+    ('개수', 'count'),
+])
+# print(yearly_stats.tail(9))
+
+apple['Month'] = apple['Date'].dt.month
+monthly_stats = apple.groupby('Month')['Close'].mean()
+# print(monthly_stats)
+
+# 분기별 수익율 분석
+apple['Quarter'] = apple['Date'].dt.quarter
+quarterly_return = apple.groupby(['Year', 'Quarter'])['Daily_Return'].sum()
+# print(quarterly_return)
+
+# 거래량이 많은 날 분석
+# print(apple['Volume'])
+apple['Volume_Category'] = pd.cut(apple['Volume'],
+                                  bins=[0, 100000000, 300_000_000, 5e8, 10e10],
+                                  labels=['Low', 'Medium', 'High', 'Very High']
+                                  )
+volume_analysis = apple.groupby(['Volume_Category'])['Close'].agg(['count', 'mean'])
+print(volume_analysis)
+
