da4a5cb82af93f02a03c318c9429afeab7218a4b
voidgogo <2021038@daelim.ac.kr>
2025-11-27 15:34:04 +0900

    v4.0 pandas

--- week14/week14_pandas.py
+++ week14/week14_pandas.py
@@ -3,32 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-pi = pd.read_csv('product_inspection.csv')
-# print(pi.head(3))
-# print(pi.info())
-# print(pi.describe())
-pi['date'] = pd.to_datetime(pi['date'])
-# print(pi.info())
-# print(pi[pi['inspection_step'] == 'B'])
-# print(pi['inspection_step'].unique())
-# print(pi.groupby('inspection_step')['value'].mean())
-# print(pi.groupby('inspection_step')['value'].describe())
-
-# 새로운 특성 추가
-pi['standard1'] = pi.groupby('inspection_step')['value'].transform(lambda x: (x - x.mean()) / x.std())
-# print(pi.info())
-# print(pi.tail())
-# print(pi.groupby('inspection_step')['value'].mean() == 0)
-# print(pi['standard1'])
-# print(pi[pi['standard1'] > 0])
-temp = pi.sort_values(['inspection_step', 'date']).drop_duplicates(['inspection_step'])
-# print(temp)
-temp = temp.set_index('inspection_step')['value']
-# print(temp)
-
-pi = pi.set_index('inspection_step')
-pi['standard2'] = pi['value'] - temp
-# print(pi)
-# print(pi['standard2'])
-pi = pi.reset_index()
-print(pi)
+pr = pd.read_csv('product.csv')
+# print(pr)
+# print(pr.info())
+# print(pr.describe())
+# print(pr.head(6))
+# print(pr.tail(3))
+# print(pr['operator'].unique())
+# print(pr['process'].unique())
+# print(pr['factory'].unique())
+pr['path'] = pr.groupby('product_id')['operator'].transform(
+    lambda x: '_'.join(x)
+)
+# print(pr.head(6))
+pr['path'] = pr['factory'] + '_' + pr['path']
+# print(pr.head(6))
+pr = pr.drop_duplicates('product_id')
+# print(pr)
+pr = pr[['date', 'product_id', 'passfail', 'path']]
+# print(pr)
+print(pr.groupby('passfail')['path'].value_counts())
